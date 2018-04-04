b8378e9450c9d857aa648af3c5336a1a162179d7
Patrick Hansen <pah@pandisign.com>
2018-04-04 13:57:56 +0200

    Added option for max rows returend

--- index.php
+++ index.php
@@ -1,51 +1,48 @@
 <?php
-
 include 'simplexlsx.class.php';
-
 class ReadFile
 {
     const INCLUDEHEADERROWS = false;
-    
+
     const INCLUDEEMPTYVALUES = false;
-    
+
     const WRITETOFILE = true;
 
     const DESTINATIONFILE = "redirects.txt";
 
     const WRITETYPE = "redirectMatch 301 ^{0}/?$ {1}"; //can be text with variables corresponding to array numeric keys or implode
 
+    const LIMITNUMBEROFROWS = 3; //This can either be false for all rows or max number of rows.
+
     public function __construct()
     {
-        $this->file = '20171030-301_redirects.xlsx';
-
+        $this->file = 'sheet.xlsx';
         if( $this->data = SimpleXLSX::parse($this->file) ){
             $this->data = $this->data;
         } else {
             $this->data = "Error could not find file.";
         }
     }
-
     private function writeToFile($txt)
     {
         file_put_contents($this::DESTINATIONFILE, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
     }
-
     public function getRows()
     {
-        return $this->data->rows();
+        $rows = $this->data->rows();
+        if($this::LIMITNUMBEROFROWS){
+            $rows = array_slice($rows,0,$this::LIMITNUMBEROFROWS);
+        }
+        return $rows;
     }
-
     public function getPreparedData()
     {
         $rows = $this->getRows();
-
         if(!$this::INCLUDEHEADERROWS){ //We dont wish to include header rows
             unset($rows[0]);
         }
-
         if($this::WRITETOFILE){
             $startTime = microtime();
-
             foreach($rows as $rk => $row)
             {
                 if(!$this::INCLUDEEMPTYVALUES){ //We dont want to include empty values so we have to run over values and see if they are empty
@@ -62,7 +59,6 @@ class ReadFile
                     $keys = array_map('intval',$matches[1]);
                     $variables = [];
                     $values = [];
-
                     foreach($keys as $key)
                     {
                         $variables[] = "{". $key ."}";
@@ -70,13 +66,11 @@ class ReadFile
                             throw new Exception("Could not find row with key: ".$key." Check that variables defined in your writetype is reflected in XLSX document.");
                         $values[] = $row[$key];
                     }
-
                     $this->writeToFile(str_replace($variables,$values,$this::WRITETYPE));
                 } else {
                     $this->writeToFile(implode(",",$row));
                 }
             }
-
             $endTime = microtime();
             return "Success! started at: ".$startTime." Ended at: ".$endTime;
         }
@@ -86,7 +80,5 @@ class ReadFile
         }
     }
 }
-
-
-//$p = new ReadFile;
-//$p->getPreparedData();
+$p = new ReadFile;
+$p->getPreparedData();
